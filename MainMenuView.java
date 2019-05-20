a443a26ce83233dcaad50920b81781340dc1975f
TED755 <geka.tihomirov@yandex.ru>
2019-05-20 21:03:46 +0300

    ended rewrite Game and menu interface

--- MainMenuView.java
+++ MainMenuView.java
@@ -22,8 +22,6 @@ public class MainMenuView extends Group {
     private Label info;
 
     private GridPane pane;
-    private GridPane one_p;
-    private GridPane two_p;
 
     private Font font;
 
@@ -50,16 +48,15 @@ public class MainMenuView extends Group {
         pane.add(player1Text, 0, 0);
         pane.add(player1, 1, 0);
 
-
         Text player2Text = new Text("Второй игрок:");
         player2Text.setFont(font);
         player2 = new Label(game.getPlayer2().getName());
         player2.setFont(font);
-        if(game.getGame_mode() == COMPHUMAN.HUMAN){
-            pane.add(player2Text, 0, 1);
-            pane.add(player2, 1, 1);
-        }
+        pane.add(player2Text, 0, 1);
+        pane.add(player2, 1, 1);
 
+        if(game.getGame_mode() == COMPHUMAN.COMPUTER)
+            player2.setText("Компьютер");
 
         info = new Label(game.toString());
         info.setFont(font);
@@ -87,79 +84,62 @@ public class MainMenuView extends Group {
             info.setScaleX(1);
             info.setScaleY(1);
         });
-        if(game.getGame_mode() == COMPHUMAN.COMPUTER){
-            if(game.getTurn() == COMPHUMAN.COMPUTER){
-                player2.setCursor(Cursor.HAND);
-                player2.setOnMouseEntered((MouseEvent e) ->{
-                    player2.setScaleX(1.15);
-                    player2.setScaleY(1.15);
 
-                });
-                player2.setOnMouseExited((MouseEvent e) ->{
-                    player2.setScaleX(1);
-                    player2.setScaleY(1);
-                });
-                player2.setOnMousePressed((MouseEvent e) ->{
-                    if(!(e.getButton() == MouseButton.SECONDARY)){
-                        NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
-                        if (nameEditDialog.getDialog().showAndWait().isPresent()) {
-                            dataChanged();
-                            System.out.println("Work");
-                        }
-                    }
-                });
+
+        player1.setCursor(Cursor.HAND);
+        player1.setOnMouseEntered((MouseEvent e) ->{
+            player1.setScaleX(1.15);
+            player1.setScaleY(1.15);
+        });
+        player1.setOnMouseExited((MouseEvent e) ->{
+            player1.setScaleX(1);
+            player1.setScaleY(1);
+        });
+        player1.setOnMousePressed((MouseEvent e) ->{
+            NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
+            if(!(e.getButton() == MouseButton.SECONDARY)){
+                if (nameEditDialog.getDialog().showAndWait().isPresent()){
+                    dataChanged();
+                    System.out.println("Work");}
             }
-            else {
-                player1.setCursor(Cursor.HAND);
-                player1.setOnMouseEntered((MouseEvent e) ->{
-                    player1.setScaleX(1.15);
-                    player1.setScaleY(1.15);
-                });
-                player1.setOnMouseExited((MouseEvent e) ->{
-                    player1.setScaleX(1);
-                    player1.setScaleY(1);
-                });
-                player1.setOnMousePressed((MouseEvent e) ->{
-                    NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
-                    if(!(e.getButton() == MouseButton.SECONDARY)){
-                        if (nameEditDialog.getDialog().showAndWait().isPresent()){
-                            dataChanged();
-                            System.out.println("Work");}
-                    }
-                });
-            }
+        });
+        setActions();
+    }
+    public void dataChanged(){
+        player1.setText(game.getPlayer1().getName());
+        player2.setText((game.getPlayer2().getName()));
+        actionsChanged();
+        info.setText(game.toString());
+    }
+
+    public void actionsChanged(){
+        if(game.getGame_mode() == COMPHUMAN.HUMAN){
+            player2.setText(game.getPlayer2().getName());
+            setActions();
         }
         else {
-            player1.setCursor(Cursor.HAND);
-            player1.setOnMouseEntered((MouseEvent e) ->{
-                player1.setScaleX(1.15);
-                player1.setScaleY(1.15);
-            });
-            player1.setOnMouseExited((MouseEvent e) ->{
-                player1.setScaleX(1);
-                player1.setScaleY(1);
-            });
-            player1.setOnMousePressed((MouseEvent e) ->{
-                NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
-                if(!(e.getButton() == MouseButton.SECONDARY)){
-                    if (nameEditDialog.getDialog().showAndWait().isPresent()) {
-                        dataChanged();
-                        System.out.println("Work");
-                    }
-                }
-            });
+            player2.setText("Компьютер");
+            setActions();
+        }
+
+    }
+
+    public void setActions(){
+        System.err.println(game.getGame_mode());
+        if(game.getGame_mode() == COMPHUMAN.HUMAN){
             player2.setCursor(Cursor.HAND);
-            player2.setOnMouseEntered((MouseEvent e) ->{
+            player2.setOnMouseEntered((e) ->{
                 player2.setScaleX(1.15);
                 player2.setScaleY(1.15);
+
             });
             player2.setOnMouseExited((MouseEvent e) ->{
                 player2.setScaleX(1);
                 player2.setScaleY(1);
             });
             player2.setOnMousePressed((MouseEvent e) ->{
-                NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
                 if(!(e.getButton() == MouseButton.SECONDARY)){
+                    NameEditDialog nameEditDialog = new NameEditDialog(game, "Сменить ник");
                     if (nameEditDialog.getDialog().showAndWait().isPresent()) {
                         dataChanged();
                         System.out.println("Work");
@@ -167,28 +147,11 @@ public class MainMenuView extends Group {
                 }
             });
         }
-        /*addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent e)->{
-            if(!(e.getButton() == MouseButton.SECONDARY)){
-                GameEditDialog gameEditDialog = new GameEditDialog(game, "Настройка");
-                if (gameEditDialog.getDialog().showAndWait().isPresent())
-                    System.out.println("Work");
-            }
-        });*/
-
-
-    }
-    public void dataChanged(){
-        player1.setText(game.getPlayer1().getName());
-        if(game.getGame_mode() == COMPHUMAN.HUMAN) {
-            player2.setText((game.getPlayer2().getName()));
+        else {
+            player2.setCursor(Cursor.DEFAULT);
+            player2.setOnMouseEntered(null);
+            player2.setOnMouseExited(null);
+            player2.setOnMousePressed(null);
         }
-        info.setText(game.toString());
-    }
-
-    public void actionsChanged(){
-        if(game.getGame_mode() == COMPHUMAN.HUMAN){
-
-        }
-
     }
 }
